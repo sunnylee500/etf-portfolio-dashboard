6b6f78e152aebcd4c1977585b751bdc404f05357
sunnylee500 <sunnylee500@gmail.com>
2020-02-07 12:32:44 -0500

    comparative dashboard

--- server.R
+++ server.R
@@ -23,9 +23,19 @@ shinyServer(function(input, output, session) {
         vt
     })
     
+    vtickers_new <- reactive({
+        vt_new <- as.character(unlist(strsplit(input$tickers_new,",")))
+        vt_new
+    })
+    
     vweights <- reactive({
         vw <- as.numeric(unlist(strsplit(input$weights,",")))
         vw
+    })
+    
+    vweights_new <- reactive({
+        vw_new <- as.numeric(unlist(strsplit(input$weights_new,",")))
+        vw_new
     })
     
     #Table of ETF info: ticker, name, L1, L2
@@ -36,19 +46,29 @@ shinyServer(function(input, output, session) {
             etf_info
     })
     
+    etf_names_new <- reactive({
+        validate(need(input$tickers_new, "Please select tickers"))
+        vt <- vtickers_new()
+        etf_info <- etfs%>%filter(Symbol%in%vt)
+        etf_info
+    })
+    
+    ######################### 
     #Table of monthly prices given the tickers 
     prices_monthly <- reactive({
         if(input$submit > 0){
-            vt <- isolate(vtickers())
-            pm <- getSymbols(vt, src='yahoo')%>%
+            vt <- vtickers()
+            vt_new <- vtickers_new()
+            both <- unique(c(vt, vt_new))
+            
+            pm <- getSymbols(both, src='yahoo')%>%
                 map(~Ad(get(.)))%>%
                 reduce(merge)%>%
                 na.omit()%>%
                 to.monthly(indexAt='last',OHLC=FALSE)%>%
-                `colnames<-`(vt)
-            pm
+                `colnames<-`(both)
         }
-    })
+     })
     
     #Table of returns given the tickers
     ticker_returns <- reactive({
@@ -56,7 +76,7 @@ shinyServer(function(input, output, session) {
             pm <- isolate(prices_monthly())
             treturns <- na.omit(Return.calculate(pm, method = "log"))
             treturns
-        }
+       }
     })
     
     #Character of shared start + end date 
@@ -72,9 +92,22 @@ shinyServer(function(input, output, session) {
     #List of risk contribution
     ticker_risks <- reactive({
         if(input$submit > 0){
-            treturns <- isolate(ticker_returns())
             vw <- isolate(vweights())
-            trisks <- StdDev(treturns, weights=vw, portfolio_method="component")
+            vt <- isolate(vtickers())
+            treturns <- isolate(ticker_returns())
+            treturns1 <- treturns[,vt]
+            trisks <- StdDev(treturns1, weights=vw, portfolio_method="component")
+            trisks
+        }
+    })
+    
+    ticker_risks_new <- reactive({
+        if(input$submit > 0){
+            vw <- isolate(vweights_new())
+            vt <- isolate(vtickers_new())
+            treturns <- isolate(ticker_returns())
+            treturns1 <- treturns[,vt]
+            trisks <- StdDev(treturns1, weights=vw, portfolio_method="component")
             trisks
         }
     })
@@ -92,51 +125,114 @@ shinyServer(function(input, output, session) {
         }
     })
     
-    # #Gather on riskreturn
-    # riskreturn2 <- reactive({
-    #     if(input$submit > 0){
-    #         rrtibble <- riskreturn()
-    #         rrtibble2 <- gather(rrtibble, key=Attribute, value, Weights:Risk, na.rm=TRUE )
-    #         rrtibble2
-    #     }
-    # })
+    riskreturn_new <- reactive({
+        if(input$submit > 0){
+            trisks <- isolate(ticker_risks_new())
+            vw <- isolate(vweights_new())
+            rrtibble <- tibble(labels(trisks$pct_contrib_StdDev),vw,trisks$pct_contrib_StdDev)%>%
+                rename('Tickers'='labels(trisks$pct_contrib_StdDev)',
+                       'Weights'='vw',
+                       'Risk'='trisks$pct_contrib_StdDev')
+            rrtibble
+        }
+    })
+    
+    
+    #Joined tables of return risk and etf info 
+    joined_rr_etfinfo <- reactive({
+        req(input$tickers)
+        req(input$weights)
+        rrtibble <- riskreturn()
+        etf_info <- etf_names()
+        ar <- left_join(rrtibble, etf_info, by=c("Tickers"="Symbol"))
+        ar
+    })
+    
+    joined_rr_etfinfo_new <- reactive({
+        req(input$tickers_new)
+        req(input$weights_new)
+        rrtibble <- riskreturn_new()
+        etf_info <- etf_names_new()
+        ar <- left_join(rrtibble, etf_info, by=c("Tickers"="Symbol"))
+        ar
+    })
     
     #Table of asset class and weight
     asset_table <- reactive({
         if(input$submit > 0){
-            vt <- isolate(vtickers())
-            etf_assets <- etfs%>%filter(Symbol%in%vt)%>%group_by(L2_Asset_Class)%>%
-                summarise(n=n())%>%mutate(ratio=n/sum(n))%>%select(L2_Asset_Class,ratio)
+            # vt <- isolate(vtickers())
+            # etf_assets <- etfs%>%filter(Symbol%in%vt)%>%group_by(L2_Asset_Class)%>%
+            #     summarise(n=n())%>%mutate(ratio=n/sum(n))%>%select(L2_Asset_Class,ratio)
+            # etf_assets
+            ar <- joined_rr_etfinfo()
+            etf_assets <- ar%>%group_by(L2_Asset_Class)%>%summarise(totalweight=sum(Weights))%>%
+                select(L2_Asset_Class,totalweight)
+            etf_assets
+        }
+    })
+    
+    asset_table_new <- reactive({
+        if(input$submit > 0){
+            # vt <- isolate(vtickers_new())
+            # etf_assets <- etfs%>%filter(Symbol%in%vt)%>%group_by(L2_Asset_Class)%>%
+            #     summarise(n=n())%>%mutate(ratio=n/sum(n))%>%select(L2_Asset_Class,ratio)
+            # etf_assets
+            ar <- joined_rr_etfinfo_new()
+            etf_assets <- ar%>%group_by(L2_Asset_Class)%>%summarise(totalweight=sum(Weights))%>%
+                select(L2_Asset_Class,totalweight)
             etf_assets
         }
     })
     
     #Table of asset class and risk contribution
     asset_risk <- reactive({
-        req(input$tickers)
-        req(input$weights)
-        rrtibble <- riskreturn()
-        etf_info <- etf_names()
-        ar <- left_join(rrtibble, etf_info, by=c("Tickers"="Symbol"))
+        # req(input$tickers)
+        # req(input$weights)
+        # rrtibble <- riskreturn()
+        # etf_info <- etf_names()
+        # ar <- left_join(rrtibble, etf_info, by=c("Tickers"="Symbol"))
+        ar <- joined_rr_etfinfo()
+        ar_table <- ar%>%group_by(L2_Asset_Class)%>%summarise(risk_byasset = sum(Risk))
+        ar_table
+    })
+    
+    asset_risk_new <- reactive({
+        # req(input$tickers_new)
+        # req(input$weights_new)
+        # rrtibble <- riskreturn_new()
+        # etf_info <- etf_names_new()
+        # ar <- left_join(rrtibble, etf_info, by=c("Tickers"="Symbol"))
+        ar <- joined_rr_etfinfo_new()
         ar_table <- ar%>%group_by(L2_Asset_Class)%>%summarise(risk_byasset = sum(Risk))
         ar_table
     })
     
     #ER of Annualized Rolling 12mo
     expected_return <- reactive({
-        pm <- prices_monthly()
+        vt <- vtickers()
         vw <- isolate(vweights())
-        annual_rolling <- na.omit(ROC(pm,n=12,type="discrete"))
+        pm <- prices_monthly()
+        pm1 <- pm[,vt]
+        annual_rolling <- na.omit(ROC(pm1,n=12,type="discrete"))
         er <- round((sum(colMeans(annual_rolling) * vw) * 100), 2) 
         er
     })
-
-
-    ################################################################
-    output$tickerlist <- renderPrint({
-        req(input$tickers)
-        vtickers()
+    
+    expected_return_new <- reactive({
+        vt <- vtickers_new()
+        vw <- isolate(vweights_new())
+        pm <- prices_monthly()
+        pm1 <- pm[,vt]
+        annual_rolling <- na.omit(ROC(pm1,n=12,type="discrete"))
+        er <- round((sum(colMeans(annual_rolling) * vw) * 100), 2) 
+        er
     })
+    
+    ################################################################
+    # output$tickerlist <- renderPrint({
+    #     req(input$tickers)
+    #     vtickers()
+    # })
     
     output$weightlist <- renderText({
         validate(need(input$tickers, "Please select tickers"),
@@ -147,8 +243,21 @@ shinyServer(function(input, output, session) {
                paste("Need to sum to 1. Number of weights needed = ",length(vtickers()),separate=""))
     })
     
+    output$weightlist_new <- renderText({
+        validate(need(input$tickers_new, "Please select tickers"),
+                 need(input$weights_new, "Please enter weights"))
+        vweights_new()
+        ifelse(((sum(vweights_new())==1) & (length(vweights_new())==length(vtickers_new()))),
+               "Great, weights sum to 1!",
+               paste("Need to sum to 1. Number of weights needed = ",length(vtickers_new()),separate=""))
+    })
+    
     output$etf_table <- renderTable({
             etf_names()%>%select(Ticker=Symbol,Name=Name, Asset.Class=L1_Asset_Class)
+    })
+    
+    output$etf_table_new <- renderTable({
+        etf_names_new()%>%select(Ticker=Symbol,Name=Name, Asset.Class=L1_Asset_Class)
     })
     
     output$time <- renderText({
@@ -161,6 +270,12 @@ shinyServer(function(input, output, session) {
         }
     })
     
+    output$exp_return_new <- renderText({
+        if(input$submit > 0){
+            expected_return_new()
+        }
+    })
+    
     output$ar_pie <- renderGvis({
         if(input$submit > 0){
             gvisPieChart(isolate(asset_risk()), labelvar="L2_Asset_Class", numvar="risk_byasset",
@@ -170,34 +285,83 @@ shinyServer(function(input, output, session) {
                                        title = "Contribution to Risk by Asset Class",
                                        titleTextStyle="{color: '#3d4854',
                                                             fontName:'Verdana',
-                                                            fontSize:13}"))
+                                                            fontSize:13}",
+                                       legend = "{position: 'left'}"))
+        }
+    })
+    
+    output$ar_pie_new <- renderGvis({
+        if(input$submit > 0){
+            gvisPieChart(isolate(asset_risk_new()), labelvar="L2_Asset_Class", numvar="risk_byasset",
+                         options = list(width =600,
+                                        height = 400,
+                                        slices = "{'0': {color: '#9f6d8a'}, '1': {color: '#7399c6'}}",
+                                        title = "Contribution to Risk by Asset Class",
+                                        titleTextStyle="{color: '#3d4854',
+                                                            fontName:'Verdana',
+                                                            fontSize:13}",
+                                        legend = "{position: 'left'}"))
         }
     })
     
     output$asset_pie <- renderGvis({
         if(input$submit > 0){
-            gvisPieChart(asset_table(), labelvar="L2_Asset_Class", numvar="ratio",
-                         options = list(width =800,
+            gvisPieChart(asset_table(), labelvar="L2_Asset_Class", numvar="totalweight",
+                         options = list(width =600,
                                        height = 400,
                                        slices = "{'0': {color: '#9f6d8a'}, '1': {color: '#7399c6'}}",
                                        title = "Portfolio Weight by Asset Class",
                                        titleTextStyle="{color: '#3d4854',
                                                             fontName:'Verdana',
-                                                            fontSize:13}"))
+                                                            fontSize:13}",
+                                       legend = "{position: 'left'}"))
+        }
+    })
+    
+    output$asset_pie_new <- renderGvis({
+        if(input$submit > 0){
+            gvisPieChart(asset_table_new(), labelvar="L2_Asset_Class", numvar="totalweight",
+                         options = list(width =600,
+                                        height = 400,
+                                        slices = "{'0': {color: '#9f6d8a'}, '1': {color: '#7399c6'}}",
+                                        title = "Portfolio Weight by Asset Class",
+                                        titleTextStyle="{color: '#3d4854',
+                                                            fontName:'Verdana',
+                                                            fontSize:13}",
+                                        legend = "{position: 'left'}"))
         }
     })
     
     output$rr_bargraph <- renderGvis({
         if(input$submit > 0){
             gvisColumnChart(riskreturn(), xvar = "Tickers", yvar = c("Weights","Risk"),
-                            options = list(width=800,
-                                           height=600,
+                            options = list(width=600,
+                                           height=500,
                                            colors = "['#fcdb6d','#da602f']",
-                                           title = "Portfolio Weights and Risk Contribution by Ticker",
+                                           title = "Weight and Risk Contribution by Ticker",
                                            titleTextStyle="{color: '#3d4854',
                                                             fontName:'Verdana',
-                                                            fontSize:15}"))
+                                                            fontSize:15}",
+                                           legend = "{position: 'bottom'}"))
         }
+    })
+    
+    output$rr_bargraph_new <- renderGvis({
+        if(input$submit > 0){
+            gvisColumnChart(riskreturn_new(), xvar = "Tickers", yvar = c("Weights","Risk"),
+                            options = list(width=600,
+                                           height=500,
+                                           colors = "['#fcdb6d','#da602f']",
+                                           title = "Weight and Risk Contribution by Ticker",
+                                           titleTextStyle="{color: '#3d4854',
+                                                            fontName:'Verdana',
+                                                            fontSize:15}",
+                                           legend = "{position: 'bottom'}"))
+        }
+    })
+    
+    output$top100_etf <- renderTable({
+        etfs%>%top_n(100)%>%mutate(Rank=c(1:100))
     })
     
 })
--- ui.R
+++ ui.R
@@ -24,6 +24,14 @@ shinyUI(fluidPage(
             font-size: 150%;
             text-align:center
          }
+        
+        #exp_return_new {
+            font-family:Verdana;
+            font-style: bold;
+            font-size: 150%;
+            text-align:center
+        } 
+         
         #time {
             font-family:Verdana;
             font-style: bold;
@@ -38,26 +46,39 @@ shinyUI(fluidPage(
         column(10,
                tags$div(
                    tags$h1("Portfolio Risk and Return"),
-                   tags$br()
-               ),
-               
+                   tags$br()),
+            tabsetPanel(type = "tabs",
+                        
+            tabPanel("Portfolio Inputs", 
                tags$div(
-                   tags$h3("Portfolio Inputs"),
-                   tags$hr()
-               ),
+                   tags$h3("Current Portfolio"),
+                   tags$hr()),
                
                fluidRow(
                    column(6,
                           tableOutput("etf_table")),
                    column(1),
                    column(3,
-                          selectInput("tickers", "Choose Tickers:", etfs[[1]], selected=c("SPY","AGG"),multiple=TRUE, selectize=TRUE),
-                          textInput("weights",label="Enter weights:", value="0.5,0.5"),
-                          textOutput("weightlist"),
-                          actionButton("submit","Submit"))
-               ),
+                          selectInput("tickers", "Current Tickers:", etfs[[1]], selected=c("SPY","AGG"),multiple=TRUE, selectize=TRUE),
+                          textInput("weights",label="Current Weights:", value="0.8,0.2"),
+                          textOutput("weightlist"))),
                
-
+               tags$div(
+                   tags$h3("Comparative Portfolio"),
+                   tags$hr()),
+               
+               fluidRow(
+                   column(6,
+                          tableOutput("etf_table_new")),
+                   column(1),
+                   column(3,
+                          selectInput("tickers_new", "Comparative Tickers:", etfs[[1]], selected=c("SPY","AGG"),multiple=TRUE, selectize=TRUE),
+                          textInput("weights_new",label="Comparative Weights:", value="0.6,0.4"),
+                          textOutput("weightlist_new"),
+                          actionButton("submit","Submit")))
+            ),
+            
+            tabPanel("Portfolio Dashboard", 
                 tags$div(
                     tags$br(),
                     tags$br(),
@@ -65,65 +86,74 @@ shinyUI(fluidPage(
                     tags$hr(),
                     tags$br()
                 ),
-               
-               fluidRow(
-                   column(10,
-                          fluidRow(
-                              column(3),
-                              column(3,
-                                     tags$div(
-                                         tags$h5("Average 1YR Rolling Returns (%)"),
-                                         tags$hr()
-                                     ),
-                                     textOutput("exp_return")),
-                              column(1),
-                              column(4,
-                                     tags$div(
-                                         tags$h5("Time Frame"),
-                                         tags$hr()
-                                     ),
-                                     textOutput("time"))
-                          )
-                   )
-               ),
-               
-               tags$div(
-                   tags$br(),
-                   tags$br()
-               ),
-               
-               fluidRow(
-                   column(1),
-                   column(9, align="center",
-                          htmlOutput("rr_bargraph")
-                   )
-               ),
-               
-               tags$div(
-                   tags$br()
-               ),
-               
-               fluidRow(
-                   column(10,
-                          fluidRow(
-                              column(4,
-                                     htmlOutput("asset_pie")
-                                     ),
-                              column(3),
-                              column(3,
-                                     htmlOutput("ar_pie")
-                                     )
-                          )
-                   )
-               ),
-               
-               tags$div(
-                   tags$br(),
-                   tags$br(),
-                   tags$br(),
-                   tags$br(),
-               )
-        ),
+                
+                fluidRow(align = "center",
+                         tags$h5("Common Time Frame"),
+                         tags$hr(width="30%"),
+                         textOutput("time"),
+                         tags$br()
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                                tags$div(align="center",
+                                         tags$h3("Current Portfolio"),
+                                         tags$hr(),
+                                         tags$br()),
+                                tags$div(align="center",
+                                         tags$h3("Comparative Portfolio"),
+                                         tags$hr(),
+                                         tags$br())
+                    )
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                           tags$div(align="center",
+                               tags$h5("Average 1YR Rolling Returns (%)"),
+                               tags$hr(width="30%")),
+                           tags$div(align="center",
+                               tags$h5("Average 1YR Rolling Returns (%)"),
+                               tags$hr(width="30%"))
+                           )
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                           textOutput("exp_return"),
+                           textOutput("exp_return_new")
+                    )
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                           htmlOutput("rr_bargraph"),
+                           htmlOutput("rr_bargraph_new"))
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                           htmlOutput("asset_pie"),
+                           htmlOutput("asset_pie_new"))
+                ),
+                
+                fluidRow(
+                    splitLayout(cellWidths = c("50%", "50%"),
+                           htmlOutput("ar_pie"),
+                           htmlOutput("ar_pie_new"))
+                )),
+            
+            tabPanel("Top 100 ETFs",
+                fluidRow(
+                    tags$div(
+                        tags$h3("Largest ETFs: Top 100 ETFs By Assets"),
+                        tags$br())
+                ),
+                fluidRow(
+                    tableOutput("top100_etf")
+                ))
+            )),
+                
         column(1)
     )
 ))
